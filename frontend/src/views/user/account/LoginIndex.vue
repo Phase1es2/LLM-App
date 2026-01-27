474e02cba334ddaa4f1645c10b2042514f00c8df
Phase1es2 <hyang17@uw.edu>
2026-01-27 11:20:58 -0500

    login and register page

--- frontend/src/views/user/account/LoginIndex.vue
+++ frontend/src/views/user/account/LoginIndex.vue
@@ -3,7 +3,21 @@
 </script>
 
 <template>
-Login
+  <div class="flex justify-center mt-30">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+      <label class="label">Username</label>
+      <input type="text" class="input" placeholder="Username" />
+
+      <label class="label">Password</label>
+      <input type="password" class="input" placeholder="Password" />
+
+      <button class="btn btn-neutral mt-4">Login</button>
+      <div class="flex justify-end">
+        <RouterLink :to="{name: 'register-index'}" class="btn btn-sm btn-ghost text-gray-50">Register</RouterLink>
+      </div>
+    </fieldset>
+  </div>
 </template>
 
 <style scoped>
--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
-Register
+  <div class="flex justify-center mt-30">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+      <label class="label">Username</label>
+      <input type="text" class="input" placeholder="Username" />
+
+      <label class="label">Password</label>
+      <input type="password" class="input" placeholder="Password" />
+
+      <label class="label">Confirm Password</label>
+      <input type="password" class="input" placeholder="Confirm Password" />
+      <button class="btn btn-neutral mt-4">Register</button>
+      <div class="flex justify-end">
+        <RouterLink :to="{name: 'login-index'}" class="btn btn-sm btn-ghost text-gray-50">Login</RouterLink>
+      </div>
+    </fieldset>
+  </div>
 </template>
 
 <style scoped>
