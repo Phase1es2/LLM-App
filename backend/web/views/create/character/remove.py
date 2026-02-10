58256b1e5e11c6d9312941d82c0dcea66ddfd263
Phase1es2 <hyang17@uw.edu>
2026-02-10 15:59:10 -0500

    update remove logic, that will remove all the photo and background image that created

--- backend/web/views/create/character/remove.py
+++ backend/web/views/create/character/remove.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from web.models.character import Character
+from web.views.utils.photo import remove_old_photos
 
 
 class RemoveCharacterView(APIView):
@@ -11,7 +12,11 @@ class RemoveCharacterView(APIView):
     def post(self, request):
         try:
             character_id = request.data['character_id']
-            Character.objects.filter(pk=character_id, author__user=request.user).delete()
+            character = Character.objects.get(pk=character_id, author__user=request.user)
+            # Character.objects.filter(pk=character_id, author__user=request.user).delete()
+            remove_old_photos(character.photo)
+            remove_old_photos(character.background_image)
+            character.delete()
             return Response({
                 'result': 'success',
             })
